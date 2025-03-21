43d582688136efc7fd89b1f82896ec5098acab50
regi <jo-eungyeong@jo-eungyeong-ui-MacBookPro.local>
2025-03-21 16:08:28 +0900

    button 공부

---
+++ app/src/main/java/com/kkyume/android/composestudy/ButtonActivity.kt
@@ -0,0 +1,140 @@
+package com.kkyume.android.composestudy
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kkyume.android.composestudy.ui.theme.ButtonTheme
+
+class ButtonActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ButtonTheme {
+                    ButtonExample(
+                        onButtonClicked = {
+                            Toast.makeText(this, "send clicked", Toast.LENGTH_SHORT).show()
+                        }
+                    )
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonExample(onButtonClicked : () -> Unit) {
+
+    // step 1: Button을 클릭했을때 Toast 출력하게 만들자.
+//    Button(onClick = onButtonClicked) {
+//        Text(text = "Send")
+//    }
+
+    // step 2: Icon을 text 앞에 추가 시켜보자.
+//    Button(onClick = onButtonClicked) {
+//        Icon(
+//            imageVector = Icons.Filled.Send,
+//            contentDescription = ""
+//        )
+//        Text(text = "Send")
+//    }
+    // step 3: 아이콘과 텍스트 사이에 spacer를 넣어보자.
+//    Button(onClick = onButtonClicked) {
+//        Icon(
+//            imageVector = Icons.Filled.Send,
+//            contentDescription = ""
+//        )
+//        Spacer(
+//            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+//        )
+//        Text(text = "Send")
+//    }
+    // step 4: enabled를 false로 바꾸자.
+//    Button(
+//        onClick = onButtonClicked,
+//        enabled = false
+//    ) {
+//        Icon(
+//            imageVector = Icons.Filled.Send,
+//            contentDescription = null
+//        )
+//        Spacer(
+//            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+//        )
+//        Text(text = "Send")
+//    }
+    //step 5: border에 BorderStroke를 설정
+//    Button(
+//        onClick = onButtonClicked,
+//        enabled = true,
+//        border = BorderStroke(10.dp, Color.Magenta)
+//    ) {
+//        Icon(
+//            imageVector = Icons.Filled.Send,
+//            contentDescription = null
+//        )
+//        Spacer(
+//            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+//        )
+//        Text(text = "Send")
+//    }
+
+    // step 6 : shape를 CircleShape로 지정
+//    Button(
+//        onClick = onButtonClicked,
+//        border = BorderStroke(10.dp, Color.Magenta),
+//        shape = CircleShape
+//    ) {
+//        Icon(
+//            imageVector = Icons.Filled.Send,
+//            contentDescription = null
+//        )
+//        Spacer(
+//            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+//        )
+//        Text(text = "Send")
+//    }
+    // step 7 : contentPadding에 PaddingValue를 설정
+    Button(
+        onClick = onButtonClicked,
+        border = BorderStroke(10.dp, Color.Magenta),
+        shape = CircleShape,
+        contentPadding = PaddingValues(20.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Send,
+            contentDescription = null
+        )
+        Spacer(
+            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+        )
+        Text(text = "Send")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ButtonTheme {
+        ButtonExample(onButtonClicked = {})
+    }
+}
